6c83d226a4a7b4d602d33856105752bf5f3a1575
Ruofeng Lei <ruofenglei@outlook.com>
2018-09-26 13:58:08 +0800

    display exist covers and attachments

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,38 +2,89 @@ const apiUrl = "https://yetian.design/ghost/api/v0.1"
 
 //get Ghost token in browser's localStorage
 var ls = JSON.parse(localStorage.getItem("ghost:session"))
+//no token, redirect to Ghost Admin page
+if (!ls) {
+    window.location.href = "/ghost"
+}
 var ghostToken = ls.authenticated.access_token
 
 //inject axios into Vue.prototype
-Vue.prototype.$axios = axios
+window.axios = axios
 //inject Ghost token into axios header
 if (ghostToken) {
-    Vue.prototype.$axios.defaults.headers.common["Authorization"] =
+    window.axios.defaults.headers.common["Authorization"] =
         "Bearer " + ghostToken
-} else {
-    window.location.href = "/ghost"
 }
 
 Vue.component("ghost-covers", {
     data: function() {
         return {
-            message: "Hello Vue"
+            message: "Covers",
+            cover: null,
+            posts: []
         }
     },
-    template: "<p>{{ message }}</p>",
-
+    template: `
+        <div>
+            <h1 class="title">{{ message }}</h1>
+            <table class="table is-bordered">
+                <thead>
+                    <th>Title</th>
+                    <th>Cover</th>
+                    <th>Visibility</th>
+                    <th>Operation</th>
+                </thead>
+                <tbody>
+                    <tr v-for="p in posts">
+                        <td>{{p.title}}</td>
+                        <td><img :src="'/content/images/cover/'+p.slug+'.png'" :alt="p.slug" style="max-width:100px"></td>
+                        <td>{{p.visibility}}</td>
+                        <td>
+                            <div class="file" style="margin-bottom:15px;">
+                                <label class="file-label">
+                                    <input class="file-input" type="file" accept="image/png" name="cover" @change="selectCover">
+                                    <span class="file-cta button is-small is-outlined">
+                                        <span class="file-label">
+                                            Change
+                                        </span>
+                                    </span>
+                                </label>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    `,
+    methods: {
+        selectCover: function(e) {
+            this.cover = e.target.files[0]
+        }
+    },
     beforeMount: function() {
-        this.$axios.get(apiUrl + "/posts", {
-            params: {
-                limit: 100,
-                fields: "title,slug,visibility"
-            }
-        }).then(function(response){
-            console.log(response.data)
-        })
+        //get all posts
+        var self = this
+        axios
+            .get(apiUrl + "/posts", {
+                params: {
+                    limit: 100,
+                    fields: "title,slug,visibility"
+                }
+            })
+            .then(function(response) {
+                self.posts = response.data.posts
+            })
     }
 })
 
 new Vue({
     el: "#cover-app"
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    //listen attachment uploader to handle upload event
+    var attInput = document.querySelector("#attachment-app input")
+    attInput.onchange = function(event) {
+        console.log(event.target.files[0])
+    }
 })
--- main.go
+++ main.go
@@ -20,33 +20,56 @@ type File struct {
 }
 
 var coverPath = ""
+var uploadPath = ""
 
 func init() {
 	if len(os.Args) < 2 {
-		log.Fatal("Where is cover path?")
+		log.Fatal("Where is Ghost content path?")
 	}
-	coverPath = os.Args[1]
+	coverPath = os.Args[1] + "/images/cover"
+	uploadPath = os.Args[1] + "/upload"
 }
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/upload", func(w http.ResponseWriter, res *http.Request) {
+	r.HandleFunc("/upload", func(w http.ResponseWriter, req *http.Request) {
 		t, _ := template.ParseFiles("./templates/index.html")
 
-		files, _ := ioutil.ReadDir(coverPath)
+		files, _ := ioutil.ReadDir(uploadPath)
 		fileInfos := []File{}
 		for _, file := range files {
 			if !file.IsDir() {
 				// fmt.Println(file.Name())
 				fileInfos = append(fileInfos, File{
 					file.Name(),
-					float64(file.Size()) / 1048576,
+					float64(file.Size()),
 					path.Ext(file.Name()),
 				})
 			}
 		}
 		t.Execute(w, fileInfos)
+	})
+
+	r.HandleFunc("/upload/api/cover", func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Write([]byte("405 Method Not Allowed"))
+			return
+		}
+		w.Write([]byte("POST: change cover api"))
+	})
+
+	r.HandleFunc("/upload/api/attachment", func(w http.ResponseWriter, req *http.Request) {
+		if req.Method == http.MethodPost {
+			w.Write([]byte("POST: attachment api"))
+		} else if req.Method == http.MethodDelete {
+			w.Write([]byte("DELETE: attachment api"))
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Write([]byte("405 Method Not Allowed"))
+			return
+		}
 	})
 
 	r.PathPrefix("/upload/assets/").Handler(http.StripPrefix("/upload/assets/", http.FileServer(http.Dir("./assets"))))
